e3a103b39a0f5fd4c95f281f1ff6a96cc8ac5d56
abhishek <abhishek.auramatics@gmail.com>
2022-09-06 18:46:08 +0530

    merge code

--- resources/views/frontend/layouts/header.blade.php
+++ resources/views/frontend/layouts/header.blade.php
@@ -1,104 +1,122 @@
-
-    <header class="ltn__header-area ltn__header-5 ltn__header-transparent--- gradient-color-4---">
-        <!-- ltn__header-top-area start -->
-        <div class="ltn__header-top-area section-bg-6 top-area-color-white---">
-            <div class="container">
-                <div class="row">
-                    <div class="col-md-7">
+<header class="ltn__header-area ltn__header-5 ltn__header-transparent--- gradient-color-4---">
+    <!-- ltn__header-top-area start -->
+    <div class="ltn__header-top-area section-bg-6 top-area-color-white---">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-7">
+                    <div class="ltn__top-bar-menu">
+                        <ul>
+                            <li><a href="mailto:info@webmail.com?Subject=Flower%20greetings%20to%20you"><i class="icon-mail"></i> info@webmail.com</a></li>
+                            <li><a href="locations.html"><i class="icon-placeholder"></i> 15/A, Nest Tower, NYC</a></li>
+                        </ul>
+                    </div>
+                </div>
+                <div class="col-md-5">
+                    <div class="top-bar-right text-end">
                         <div class="ltn__top-bar-menu">
                             <ul>
-                                <li><a href="mailto:info@webmail.com?Subject=Flower%20greetings%20to%20you"><i class="icon-mail"></i> info@webmail.com</a></li>
-                                <li><a href="locations.html"><i class="icon-placeholder"></i> 15/A, Nest Tower, NYC</a></li>
+                                <li class="d-none">
+                                    <!-- ltn__language-menu -->
+                                    <div class="ltn__drop-menu ltn__currency-menu ltn__language-menu">
+                                        <ul>
+                                            <li><a href="#" class="dropdown-toggle"><span class="active-currency">English</span></a>
+                                                <ul>
+                                                    <li><a href="#">Arabic</a></li>
+                                                    <li><a href="#">Bengali</a></li>
+                                                    <li><a href="#">Chinese</a></li>
+                                                    <li><a href="#">English</a></li>
+                                                    <li><a href="#">French</a></li>
+                                                    <li><a href="#">Hindi</a></li>
+                                                </ul>
+                                            </li>
+                                        </ul>
+                                    </div>
+                                </li>
+                                <li>
+                                    <!-- ltn__social-media -->
+                                    <div class="ltn__social-media">
+                                        <ul>
+                                            <li><a href="#" title="Facebook"><i class="fab fa-facebook-f"></i></a></li>
+                                            <li><a href="#" title="Twitter"><i class="fab fa-twitter"></i></a></li>
+
+                                            <li><a href="#" title="Instagram"><i class="fab fa-instagram"></i></a></li>
+                                            <li><a href="#" title="Dribbble"><i class="fab fa-dribbble"></i></a></li>
+                                        </ul>
+                                    </div>
+                                </li>
+                                <li>
+                                    <!-- header-top-btn -->
+                                    <div class="header-top-btn">
+                                        <a href="add-listing.html">Add Listing</a>
+                                    </div>
+                                </li>
+                                <div class="ltn__drop-menu user-menu">
+                                    <ul>
+                                        <li>
+                                            <a href="#"><i class="icon-user"></i></a>
+                                            <ul>
+                                                @if(!Auth::check())
+                                                <li><a href="{{route('login')}}">Sign in</a></li>
+                                                <li><a href="{{route('registration')}}">Register</a></li>
+                                                @else
+                                                <li><a href="{{route('buyer.dashboard')}}">Dashboard</a></li>
+                                                <li><a href="{{ route('logout') }}" onclick="event.preventDefault();
+                                                document.getElementById('logout-form').submit();">
+                                                        Sign Out
+                                                    </a></li>
+                                                @endif
+                                            </ul>
+                                        </li>
+                                    </ul>
+                                </div>
                             </ul>
-                        </div>
-                    </div>
-                    <div class="col-md-5">
-                        <div class="top-bar-right text-end">
-                            <div class="ltn__top-bar-menu">
-                                <ul>
-                                    <li class="d-none">
-                                        <!-- ltn__language-menu -->
-                                        <div class="ltn__drop-menu ltn__currency-menu ltn__language-menu">
-                                            <ul>
-                                                <li><a href="#" class="dropdown-toggle"><span class="active-currency">English</span></a>
-                                                    <ul>
-                                                        <li><a href="#">Arabic</a></li>
-                                                        <li><a href="#">Bengali</a></li>
-                                                        <li><a href="#">Chinese</a></li>
-                                                        <li><a href="#">English</a></li>
-                                                        <li><a href="#">French</a></li>
-                                                        <li><a href="#">Hindi</a></li>
-                                                    </ul>
-                                                </li>
-                                            </ul>
-                                        </div>
-                                    </li>
-                                    <li>
-                                        <!-- ltn__social-media -->
-                                        <div class="ltn__social-media">
-                                            <ul>
-                                                <li><a href="#" title="Facebook"><i class="fab fa-facebook-f"></i></a></li>
-                                                <li><a href="#" title="Twitter"><i class="fab fa-twitter"></i></a></li>
-                                                
-                                                <li><a href="#" title="Instagram"><i class="fab fa-instagram"></i></a></li>
-                                                <li><a href="#" title="Dribbble"><i class="fab fa-dribbble"></i></a></li>
-                                            </ul>
-                                        </div>
-                                    </li>
-                                    <li>
-                                        <!-- header-top-btn -->
-                                        <div class="header-top-btn">
-                                            <a href="add-listing.html">Add Listing</a>
-                                        </div>
-                                    </li>
-                                </ul>
-                            </div>
                         </div>
                     </div>
                 </div>
             </div>
         </div>
-        <!-- ltn__header-top-area end -->
-        
-        <!-- ltn__header-middle-area start -->
-        <div class="ltn__header-middle-area ltn__header-sticky ltn__sticky-bg-white">
-            <div class="container">
-                <div class="row">
-                    <div class="col">
-                        <div class="site-logo-wrap">
-                            <div class="site-logo">
-                                <a href="{{route('index')}}"><img src="{{asset('frontend/img/logo.png')}}" alt="Logo"></a>
+    </div>
+    <!-- ltn__header-top-area end -->
+
+    <!-- ltn__header-middle-area start -->
+    <div class="ltn__header-middle-area ltn__header-sticky ltn__sticky-bg-white">
+        <div class="container">
+            <div class="row">
+                <div class="col">
+                    <div class="site-logo-wrap">
+                        <div class="site-logo">
+                            <a href="{{route('index')}}"><img src="{{asset('frontend/img/logo.png')}}" alt="Logo"></a>
+                        </div>
+                        <div class="get-support clearfix d-none">
+                            <div class="get-support-icon">
+                                <i class="icon-call"></i>
                             </div>
-                            <div class="get-support clearfix d-none">
-                                <div class="get-support-icon">
-                                    <i class="icon-call"></i>
-                                </div>
-                                <div class="get-support-info">
-                                    <h6>Get Support</h6>
-                                    <h4><a href="tel:+123456789">123-456-789-10</a></h4>
-                                </div>
+                            <div class="get-support-info">
+                                <h6>Get Support</h6>
+                                <h4><a href="tel:+123456789">123-456-789-10</a></h4>
                             </div>
                         </div>
                     </div>
-                    <div class="col header-menu-column">
-                        <div class="header-menu d-none d-xl-block">
-                            <nav>
-                                <div class="ltn__main-menu">
-                                    <ul>
-                                        <li><a href="{{route('home')}}">Home</a></li>
-                                        <li><a href="{{route('property_list')}}">Properties</a></li>
-                                        <li><a href="{{route('index')}}">Home Loans</a></li>
-                                        <li><a href="{{route('about_us')}}">About</a></li>
-                                        
-                                        <li><a href="{{route('contact_us')}}">Contact</a></li>
-                                    </ul>
-                                </div>
-                            </nav>
-                        </div>
+                </div>
+                <div class="col header-menu-column">
+                    <div class="header-menu d-none d-xl-block">
+                        <nav>
+                            <div class="ltn__main-menu">
+                                <ul>
+                                    <li><a href="{{route('home')}}">Home</a></li>
+                                    <li><a href="{{route('property_list')}}">Properties</a></li>
+                                    <li><a href="{{route('index')}}">Home Loans</a></li>
+                                    <li><a href="{{route('about_us')}}">About</a></li>
+
+                                    <li><a href="{{route('contact_us')}}">Contact</a></li>
+                                </ul>
+                            </div>
+                        </nav>
                     </div>
-                    <div class="col ltn__header-options ltn__header-options-2 mb-sm-20">
-                        <!-- header-search-1 -->
-                        <div class="header-search-wrap">
+                </div>
+                <div class="col ltn__header-options ltn__header-options-2 mb-sm-20">
+                  {{--  <!-- header-search-1 -->
+                    <!-- <div class="header-search-wrap">
                             <div class="header-search-1">
                                 <div class="search-icon">
                                     <i class="icon-search for-search-show"></i>
@@ -113,9 +131,9 @@
                                     </button>
                                 </form>
                             </div>
-                        </div>
-                        <!-- user-menu -->
-                        <div class="ltn__drop-menu user-menu">
+                        </div> -->
+                    <!-- user-menu -->
+                    <!-- <div class="ltn__drop-menu user-menu">
                             <ul>
                                 <li>
                                     <a href="#"><i class="icon-user"></i></a>
@@ -134,33 +152,33 @@
                                     </ul>
                                 </li>
                             </ul>
-                        </div>
-                        <!-- mini-cart -->
-                        <div class="mini-cart-icon">
+                        </div> -->
+                    <!-- mini-cart -->
+                    <!-- <div class="mini-cart-icon">
                             <a href="#ltn__utilize-cart-menu" class="ltn__utilize-toggle">
                                 <i class="icon-shopping-cart"></i>
                                 <sup>2</sup>
                             </a>
-                        </div>
-                        <!-- mini-cart -->
-                        <!-- Mobile Menu Button -->
-                        <div class="mobile-menu-toggle d-xl-none">
-                            <a href="#ltn__utilize-mobile-menu" class="ltn__utilize-toggle">
-                                <svg viewBox="0 0 800 600">
-                                    <path d="M300,220 C300,220 520,220 540,220 C740,220 640,540 520,420 C440,340 300,200 300,200" id="top"></path>
-                                    <path d="M300,320 L540,320" id="middle"></path>
-                                    <path d="M300,210 C300,210 520,210 540,210 C740,210 640,530 520,410 C440,330 300,190 300,190" id="bottom" transform="translate(480, 320) scale(1, -1) translate(-480, -318) "></path>
-                                </svg>
-                            </a>
-                        </div>
+                        </div> -->
+                    <!-- mini-cart -->
+                    <!-- Mobile Menu Button --> --}}
+                    <div class="mobile-menu-toggle d-xl-none">
+                        <a href="#ltn__utilize-mobile-menu" class="ltn__utilize-toggle">
+                            <svg viewBox="0 0 800 600">
+                                <path d="M300,220 C300,220 520,220 540,220 C740,220 640,540 520,420 C440,340 300,200 300,200" id="top"></path>
+                                <path d="M300,320 L540,320" id="middle"></path>
+                                <path d="M300,210 C300,210 520,210 540,210 C740,210 640,530 520,410 C440,330 300,190 300,190" id="bottom" transform="translate(480, 320) scale(1, -1) translate(-480, -318) "></path>
+                            </svg>
+                        </a>
                     </div>
                 </div>
             </div>
         </div>
-        <!-- ltn__header-middle-area end -->
-    </header>
+    </div>
+    <!-- ltn__header-middle-area end -->
+</header>
 
-    
+
 <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
     @csrf
 </form>
