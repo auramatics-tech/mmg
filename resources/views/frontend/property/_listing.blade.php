53455ad9015c23162439e558a4c502d082e58481
Sukhwinder Sodhi <38687401+sukh16@users.noreply.github.com>
2022-12-22 14:07:46 +0530

    code merge

--- resources/views/frontend/property/_listing.blade.php
+++ resources/views/frontend/property/_listing.blade.php
@@ -3,7 +3,6 @@
         <div class="ltn__product-tab-content-inner ltn__product-grid-view">
             <div class="row">
                 <div class="col-lg-12">
-                    <!-- Search Widget -->
                     <div class="ltn__search-widget mb-30">
                         <form action="#" id="form1">
                             <input type="text" name="search" placeholder="Search your keyword...">
@@ -24,13 +23,6 @@
                             </div>
                         </div>
                         <div class="product-info">
-                                {{--
-                                <div class="product-badge">
-                                    <ul>
-                                        <li class="sale-badg">{{isset($property->status)?$property->status:''}}</li>
-                                    </ul>
-                                </div>
-                                --}}
                                 @if($property->form_type == 'residential' ||$property->form_type == 'residential_rental' ||$property->form_type == 'residential_sale')
                                 <h2 class="product-title"><a href="{{route('property_details',$property->id)}}">{{isset($property->property_type)?$property->property_type:''}}</a></h2>
                                 @endif
@@ -122,10 +114,6 @@
         <div class="ltn__product-tab-content-inner ltn__product-list-view">
             <div class="row">
                 <div class="col-lg-12">
-                    <!-- Search Widget -->
-
-                    <!-- Search Widget -->
-
                     <div class="ltn__search-widget mb-30">
                         <form action="#" id="form1">
                             <input type="text" name="search" placeholder="Search your keyword...">
@@ -133,7 +121,6 @@
                         </form>
                     </div>
                 </div>
-                <!-- ltn__product-item -->
                 @if(isset($properties) && count($properties))
                 @foreach($properties as $property_list)
                 <div class="col-lg-12">
@@ -176,7 +163,6 @@
                             <ul>
                                 <li class="su_fav_proporty_logo" data-property_id="{{$property_list->id}}">
                                     <a href="javascript:" data-property_id="{{$property_list->id}}" class="fav_property">
-                                        {{-- <i class="{{check_favourite_property($property_list->id)?'fa':'far'}} fa-heart"></i>--}}
                                         <img  src="{{check_favourite_property($property_list->id)? asset('frontend/img/my_logo/png/MMG_Gold_1.png') : asset('frontend/img/my_logo/png/MMG_Black_2.png')}}" alt="Logo">
                                     </a>
                                 </li>
